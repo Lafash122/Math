e3cf5d73d0c450c6a71bf6e42d501224a7a85345
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2024-05-06 01:44:50 +0700

    quadraticequation.py

--- quadraticequation.py
+++ quadraticequation.py
@@ -1,13 +1,17 @@
 a, b, c = map (float, input("Enter the coefficients value: ") .split())
 D = b**2 - 4*a*c
-if a == 1:
+if a == 1 or ((b % a == 0) and (c % a == 0)):
 	print ("It's a reduced quadratic equation")
 else:
 	print ("It's not a reduced quadratic equation")
+
+print (f"Discriminant = {D}")
+
 if D > 0:
         import math
         x1 = (-b + math.sqrt(D))/(2*a)
         x2 = (-b - math.sqrt(D))/(2*a)
+	
         print (f"The square root of the discriminant is {math.sqrt(D)}")
         print (f"x1 = {x1}")
         print (f"x2 = {x2}")
