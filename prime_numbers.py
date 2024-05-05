cbb975975f9ba32f88cd2075f31eb420fe2ce4c8
Lafash122 <120730181+Lafash122@users.noreply.github.com>
2024-05-06 01:31:23 +0700

    Add files via upload

---
+++ prime_numbers.py
@@ -0,0 +1,54 @@
+def first_primes(n):
+    a = []
+    k = 2
+    while n > 0:
+        st = 0
+        for i in range(2, round(k**0.5)+1):
+            if k % i == 0:
+                st = 1
+                break
+        if st == 0:
+            a.append(k)
+            n -= 1
+        k += 1
+    return a
+
+def not_bigger_primes(n):
+    a = []
+    k = 2
+    while n >= k:
+        st = 0
+        for i in range(2, round(k**0.5)+1):
+            if k % i == 0:
+                st = 1
+                break
+        if st == 0:
+            a.append(k)
+        k += 1
+    return a
+
+def is_prime(n):
+    for i in range(2, round(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def near_least_prime(n):
+    k = n - 1
+    while not is_prime(k):
+        k -= 1
+    return k
+
+def near_bigger_prime(n):
+    k = n + 1
+    while not is_prime(k):
+        k += 1
+    return k
+    
+
+n = int(input("Number of prime numbers: "))
+print(first_primes(n))
+print(not_bigger_primes(n))
+print(is_prime(n))
+print(near_least_prime(n))
+print(near_bigger_prime(n))
